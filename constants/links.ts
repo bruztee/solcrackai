70656c0c1f4c7fffc8c0bd548226b5874ef8c9ad
bruztee <diary3405@gmail.com>
2025-03-04 08:45:02 +0200

    Add files via upload

---
+++ constants/links.ts
@@ -0,0 +1,54 @@
+// Social Media Links
+export const SOCIAL_LINKS = {
+  twitter: "https://twitter.com/solcrackai",
+  github: "https://github.com/bruztee/solcrackai",
+  discord: "https://twitter.com/solcrackai",
+  telegram: "https://t.me/solcrackai",
+};
+
+// Documentation Links
+export const DOC_LINKS = {
+  apiReference: "/docs#api",
+  gettingStarted: "/docs#getting-started",
+  examples: "/docs#examples",
+  tutorials: "/blog/tutorials",
+  changelog: "/blog/changelog",
+};
+
+// Community Links
+export const COMMUNITY_LINKS = {
+  forum: "https://twitter.com/solcrackai",
+  events: "/community#events",
+  showcase: "/community#showcase",
+  ambassadors: "/community#ambassadors",
+  contribute: "/community#contribute",
+};
+
+// Company Links
+export const COMPANY_LINKS = {
+  about: "/about",
+  careers: "/careers",
+  contact: "/contact",
+  privacy: "/privacy",
+  terms: "/terms",
+  cookies: "/cookies",
+};
+
+// App Links
+export const APP_LINKS = {
+  launch: "/launch",
+  demo: "/demo",
+  pricing: "/pricing",
+  docs: "/docs",
+  community: "/community",
+};
+
+// Section IDs for in-page navigation
+export const SECTION_IDS = {
+  features: "features",
+  builder: "builder",
+  solana: "solana",
+  events: "events",
+  showcase: "showcase",
+  contribute: "contribute",
+};
